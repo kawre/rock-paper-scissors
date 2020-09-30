95ec3cd76f0e3aff5c4712867188c96030abd076
kawre <69250723+kawre@users.noreply.github.com>
2020-09-30 21:30:42 +0200

    base js

--- app.js
+++ app.js
@@ -8,12 +8,40 @@ btn.forEach(function (e) {
     const yourPick = document.querySelector(".you");
     pickCont.classList.add("hide");
     resultCont.classList.add("show");
+    resultCont.classList.remove("hide");
 
     yourPick.innerHTML = `<h2>YOU PICKED</h2>
-    <button class="${id}-container cont">
+    <button class="${id}-container cont animation">
     <div class="${id} img">
       <img src="/images/icon-${id}.svg" alt="" />
     </div>
-  </button>`;
+    </button>`;
+
+    const housePicks = ["rock", "paper", "scissors"];
+
+    let i = Math.floor(Math.random() * housePicks.length);
+    const housePicked = housePicks[i];
+
+    const housePick = document.querySelector(".house button");
+    housePick.classList.add("animation");
+
+    setTimeout(function housePick() {
+      const houseCont = document.querySelector(".house");
+
+      houseCont.innerHTML = `<h2>THE HOUSE PICKED</h2>
+      <button class="${housePicked}-container cont">
+        <div class="${housePicked} img">
+          <img src="/images/icon-${housePicked}.svg" alt="" />
+        </div>
+      </button>`;
+      resultCont.classList.add("final");
+    }, 1200);
+    function results() {
+      const resultsText = document.querySelector(".results");
+      if (id == housePicked) {
+        resultsText.innerHTML = "DRAW";
+      }
+    }
+    results();
   });
 });
